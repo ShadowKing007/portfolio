864cde08abb9df09a9a1305ef32340bd886c3ddd
ShadowKing007 <shariffibrahim528@gmail.com>
2024-05-27 21:20:07 +0530

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Ibrahim's Portfolio",
   description: "Modern and Minimalist Portfolio",
-  image: "/public/Portfolio.png",
 };
 
 export default function RootLayout({
@@ -21,8 +20,7 @@ export default function RootLayout({
       <head>
         <meta property="og:title" content="Ibrahim's Portfolio" />
         <meta property="og:description" content="Modern and Minimalist Portfolio" />
-        {/* Update the og:image content to contain the URL of your Open Graph image */}
-        <meta property="og:image" content={`https://ibrahim-web-portfolio.vercel.app/${metadata.image}`} />
+        <meta property="og:image" content="/opengraph-image.png" />
         <link rel="icon" href="SI-black-logo.png" sizes="576px" />
       </head>
       <body className={inter.className}>
