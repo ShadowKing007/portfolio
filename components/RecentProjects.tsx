0ead8a10b3f625082d3ab0d5b7a20acd9b33ddbe
ShadowKing007 <shariffibrahim528@gmail.com>
2024-05-27 19:11:20 +0530

    Successfully developed my Personal Portfolio

---
+++ components/RecentProjects.tsx
@@ -0,0 +1,56 @@
+// RecentProjects.tsx
+import { projects } from '@/data';
+import React from 'react';
+import { PinContainer } from './ui/3d-pin';
+import { FaLocationArrow } from 'react-icons/fa6';
+
+const RecentProjects: React.FC = () => {
+  return (
+    <div className="py-20" id="projects">
+      <h1 className="heading">
+        A Small Selection of <span className="text-purple">Recent Projects</span>
+      </h1>
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
+        {projects.map(({ id, title, des, img, iconLists, link }) => (
+          <div
+            key={id}
+            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
+          >
+            <PinContainer title={link} href={link}>
+              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden h-[30vh] sm:h-[40vh] mb-10">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-black-100"></div>
+                <img src={img} alt={title} className="absolute z-10 bottom-0" />
+              </div>
+
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {title}
+              </h1>
+              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+                {des}
+              </p>
+              <div className="flex items-center justify-between mt-7 bt-3">
+                <div className="flex items-center">
+                  {iconLists.map((icon, index) => (
+                    <div
+                      key={icon}
+                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{ transform: `translateX(-${10 * index}px)` }}
+                    >
+                      <img src={icon} alt={icon} className="p-2" />
+                    </div>
+                  ))}
+                </div>
+                <div className="flex justify-center items-center">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">Check Live Site</p>
+                  <FaLocationArrow className="ms-3" color="#CBACF9" />
+                </div>
+              </div>
+            </PinContainer>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecentProjects;
---
+++ data/index.ts
@@ -0,0 +1,157 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Experience", link: "#experience" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently searching for a full time Job",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Nike Shoes Landing Page",
+      des: "This project showcases modern web development techniques to create an engaging user experience with responsive design, smooth animations, and intuitive navigation.",
+      img: "/Nike.png",
+      iconLists: ["/re.svg", "/tail.svg", "/javascript.svg", ],
+      link: "https://nike-landing-page-ten-dusky.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "MovieSpace - Movies database",
+      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+      img: "/MovieSpace.png",
+      iconLists: ["/re.svg", "/tail.svg", "/javascript.svg", ],
+      link: "https://movie-space-five.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "RecipeShare - Indian Recipes database",
+      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+      img: "/Recipeshare.png",
+      iconLists: ["/html5.svg", "/css3.svg", "/javascript.svg","/bootstrap.svg", "/python.svg", "/mysql.svg"],
+      link: "https://github.com/ShadowKing007/RecipeShare",
+    },
+    {
+      id: 4,
+      title: "Building...",
+      des: "A new project is yet to be build. If you have any idea, email me and let's make it real.",
+      img: "/Building.png",
+      iconLists: [],
+      link: "",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Software Analyst Intern",
+      desc: "Assisted in the development of a web-based platform contributing to enhanced functionality and user experience.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Full Stack Developer - Mini Project",
+      desc: "Designed and developed Web pages for Indian Recipes data using Python Flask and MySQL Database.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance Web Dev Project",
+      desc: "Led the dev of a website for a client, from initial concept to deployment using Frontend tools.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed high-performance landing page, providing users with an engaging and immersive experience.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      name:'Github',
+      img: "/git.svg",
+      link:'https://github.com/ShadowKing007' 
+    },
+    {
+      id: 2,
+      name:'Twitter',
+      img: "/twit.svg",
+      link:'https://twitter.com/ShadowKingOOO7' 
+    },
+    {
+      id: 3,
+      name:'LinkedIn',
+      img: "/link.svg",
+      link:'https://www.linkedin.com/in/sk-ibrahim-shariff/' 
+    },
+  ];
